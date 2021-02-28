940c1610552ce3af2904be9490771149cd979a5b
Everton Dantas <evertonbonfim@yahoo.com.br>
2021-02-28 15:09:08 -0300

    Banco de dados h2 e criacao automatica da base de dados

--- src/main/java/com/educandoweb/curso/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/curso/resources/CategoriaResource.java
@@ -13,23 +13,22 @@ import com.educandoweb.curso.domain.Categoria;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar( ) {
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	public List<Categoria> listar() {
+	 Categoria cat1 = new Categoria(1, "Informática");
+	 Categoria cat2 = new Categoria(2, "Escritório");
+
+	 List<Categoria> lista = new ArrayList<>();
+	 lista.add(cat1);
+	 lista.add(cat2);
+
+	 return lista;
+	 }
 		
 		
 		
 		
 	}
 
-}
+
 
