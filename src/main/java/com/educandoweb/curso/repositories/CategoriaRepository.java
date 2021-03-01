a37f8be741c7615af46fe5f1d564b36b7e26d78f
Everton Dantas <evertonbonfim@yahoo.com.br>
2021-03-01 14:25:15 -0300

    Criando operacao de instanciacao

--- src/main/java/com/educandoweb/curso/repositories/CategoriaRepository.java
+++ src/main/java/com/educandoweb/curso/repositories/CategoriaRepository.java
@@ -3,10 +3,13 @@ package com.educandoweb.curso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import com.educandoweb.curso.domain.Categoria;
 
 @Repository
 public interface  CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
 	
 	
 
